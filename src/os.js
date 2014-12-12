c6208ed9f1299fbc2aca1837c212bc09a5403b10
donghanji <l.x-xiong@163.com>
2014-12-12 10:07:09 +0800

    update os.js plugin

--- src/os.js
+++ src/os.js
@@ -19,6 +19,16 @@ Currently  is only to userAgent
         DOLFIN : /(dolfin)(?:.*version|)[\/]([\w.]+)/, //
         SILK : /(silk)(?:.*version|)[\/]([\w.]+)/, //
         UC : /(uc)browser(?:.*version|)[\/]([\w.]+)/, //
+        TAOBAO : /(tao|taobao)browser(?:.*version|)[\/]([\w.]+)/,
+        LIEBAO : /(lb)browser(?:.*? rv:([\w.]+)|)/,
+        
+        //AMAYA:/(amaya)[\/]([\w.]+)/,
+        //SEAMONKEY:/(seamonkey)[\/]([\w.]+)/,
+        //OMNIWEB:/(omniweb)[\/]v([\w.]+)/,
+        //FLOCK:/(flock)[\/]([\w.]+)/,
+        //EPIPHANY:/(epiphany)[\/]([\w.]+)/,
+        
+        MicroMessenger :/micromessenger/i,//
         
         //engine
         WEBKIT : /webkit[\/]([\w.]+)/,
@@ -30,13 +40,15 @@ Currently  is only to userAgent
         MAC : /(mac os x)\s+([\w_]+)/, //
         WINNDOWS : /(windows nt)\s+([\w.]+)/, //
         LINUX : /linux/, //
-        IOS : /i(?:pad|phone|pod)(?:.*)cpu(?: iphone)? os/,
+        //IOS : /i(?:pad|phone|pod)(?:.*)cpu(?: iphone)? os/,
+        IOS : /(i(?:pad|phone|pod))(?:.*)cpu(?: i(?:pad|phone|pod))? os (\d+(?:[\.|_]\d+){1,})/,
         ANDROID : /(android)\s+([\d.]+)/,
         WINDOWSPHONE : /windowsphone/, //
         IPAD : /(ipad).*os\s([\d_]+)/,
         IPHONE : /(iphone\sos)\s([\d_]+)/,
+        IPOD : /(ipod)(?:.*)cpu(?: iphone)? os (\d+(?:[\.|_]\d+){1,})/,
         TOUCHPAD : /touchpad/,
-        BLACKBERRY : /(blackberry|bb\d+).*version\/([\d.]+)/,
+        BLACKBERRY : /(playbook|blackberry|bb\d+).*version\/([\d.]+)/,
         RIMTABLET : /rimtablet/, //
         BADA : /bada/, //
         CHROMEOS : /cromeos///
@@ -55,6 +67,8 @@ Currently  is only to userAgent
             dolphi = ua.match(Exp_USERAGENT.DOLFIN),
             silk = ua.match(Exp_USERAGENT.SILK),
             uc = ua.match(Exp_USERAGENT.UC),
+            taobao=ua.match(Exp_USERAGENT.TAOBAO),
+            liebao=ua.match(Exp_USERAGENT.LIEBAO),
             
             //engine
             webkit = ua.match(Exp_USERAGENT.WEBKIT),
@@ -75,6 +89,7 @@ Currently  is only to userAgent
             
             //mobile
             ios = ua.match(Exp_USERAGENT.IOS),
+            ipod = ua.match(Exp_USERAGENT.IPOD),
             iphone = !ipad && ua.match(Exp_USERAGENT.IPHONE),
             android = ua.match(Exp_USERAGENT.ANDROID),
             windowsphone = ua.match(Exp_USERAGENT.WINDOWSPHONE),
@@ -91,38 +106,48 @@ Currently  is only to userAgent
             if (trident)
                 os.trident = true;
             //device
+            os['device'] = {};
             if (mac)
-                os.mac = true, os.version = os['device-version'] = mac[2];
+                os.mac = true, os['device']['name'] = 'mac os', os.version = mac[2];
             if (windows)
-                os.windows = true, os.version = os['device-version'] = windows[2];
+                os.windows = true, os['device']['name'] = 'window', os.version = windows[2];
             if (linux)
-                os.linux; //
+                os.linux = true, os['device']['name'] = 'linux'; //
             if (chromeos)
-                os.chromeos = true;
+                os.chromeos = true, os['device']['name'] = 'chromeos', os.version = chromeos[2];
             
             //if (ios) os.ios=true;//
             if (android)
-                os.android = true, os.version = os['device-version'] = android[2];
+                os.android = true, os['device']['name'] = 'android', os.version = android[2];
             if (iphone)
-                os.ios = true, os.version = os['device-version'] = iphone[2].replace(/_/g, '.'), os.iphone = true;
+                os.ios = true, os['device']['name'] = 'iphone',os.version = iphone[2].replace(/_/g, '.'), os.iphone = true;
+            if (ipod)
+                os.ios = true, os['device']['name'] = 'ipod',os.version = ipod[2].replace(/_/g, '.'), os.ipod = true;
             if (ipad)
-                os.ios = true, os.version = os['device-version'] = ipad[2].replace(/_/g, '.'), os.ipad = true;
+                os.ios = true, os['device']['name'] = 'ipad', os.version = ipad[2].replace(/_/g, '.'), os.ipad = true;
             if (webos)
-                os.webos = true, os.version = os['device-version'] = webos[2];
+                os.webos = true, os['device']['name'] = 'webos',os.version = webos[2];
             if (blackberry)
-                os.blackberry = true, os.version = os['device-version'] = blackberry[2];
+                os.blackberry = true, os['device']['name'] = 'blackberry', os.version = blackberry[2];
             if (bada)
-                os.bada = true, os.version = ''; //
+                os.bada = true, os['device']['name'] = 'bada',os.version = ''; //
             
             if (rimtablet)
-                os.rimtablet = true, os.version = ''; //
+                os.rimtablet = true, os['device']['name'] = 'rimtablet', os.version = ''; //
             if (touchpad)
-                os.touchpad = true, os.version = ''; //
+                os.touchpad = true, os['device']['name'] = 'touchpad',os.version = ''; //
+            //os.device.version
+            os['device']['version'] = os.version;
             
-            if (!(android || iphone || ipad || webos || blackberry || bada || rimtablet || touchpad))
+            if (!(android || iphone || ipad || ipod || webos || blackberry || bada || rimtablet || touchpad))
                 os.desktop = true, os.version = '';
             //browser
-            var match = dolphi || silk || uc || msie || opera || chrome || safari || (ua.indexOf('compatible') < 0 && mozilla);
+            var match = dolphi || silk || uc || msie || taobao || liebao || opera || chrome || safari || (ua.indexOf('compatible') < 0 && mozilla);
+            //chrome
+            match[1] = match[1] === 'crios' ? 'chrome' : match[1];
+            //taobao
+            match[1] = match[1] === 'tao' ? 'taobao' : match[1];
+            
             os[match[1]] = true;
             os['browser'] = match[1];
             os['version'] = match[2]||'';
@@ -132,11 +157,20 @@ Currently  is only to userAgent
             //revise
             //safari
             if (os.ios && os.webkit && !os.desktop) {
-				
-                os.safari = (window.canSetSearchEngine || window.TrackEvent) ? true : false;//TODO
+                try{
+                    //in node js
+                    os.safari = (window.canSetSearchEngine || window.TrackEvent) ? true : false;
+                }catch(e){}
                 var v=os['major']||parseInt(os['device-version'],10)||'';
                 v && (os['ios'+v]=true);
             }
+            //ie 11
+            if(os.trident && os.major >= 11){
+                os.browser='msie';
+                os.msie=true;
+                delete os.mozilla;
+            }
+            
             //mozilla/firefox
             if (os.mozilla) {
                 os.firefox = true;
@@ -147,14 +181,32 @@ Currently  is only to userAgent
                 os.opera=os.opr;
             }
             
-            //chrome
-            os.browser = os.browser === 'crios' ? 'chrome' : os.browser;
-            
+            //blackberry
+            if(os.blackberry){
+                
+                delete os.safari;
+            }
+            //MicroMessager
+            if(Exp_USERAGENT.MicroMessenger.test(ua)){
+                
+                os.micromessage=true;
+            }
             //uc
             var DOMWindow = DOMWindow || {};
             if (DOMWindow && DOMWindow.UCNewsJSController) {
                 os.uc = true,
                 os.browser = 'uc';
+            }
+            
+            try{
+                //orientation
+                os.orientation = (window.orientation === 180 || window.orientation === 0) ? 'portrait' : 'landscape';
+            }catch(e){}
+            //os.device.type
+            if(os.desktop){
+                os.device['type']='desktop';
+            }else{
+                os.device['type']='mobile';
             }
             
             return os;
